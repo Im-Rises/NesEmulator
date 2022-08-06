869ed96e5fd99a9ca80d078d6edcf408605ea7bf
Im-Rises <quentin-morel88@hotmail.com>
2022-08-06 11:12:22 +0200

    Added SFML statically and dynamically

--- NesEmulator/main.cpp
+++ NesEmulator/main.cpp
@@ -1,21 +1,25 @@
-#include <SFML/Window.hpp>
+#include <iostream>
 
-int main()
+#include "Nes.h"
+
+/// <summary>
+/// Errors : 
+/// 1 - Target rom not found
+/// 2 - Unknown cartridge (mapper not implemented)
+/// </summary>
+/// <param name="argc"></param>
+/// <param name="argv"></param>
+/// <returns></returns>
+
+int main(int argc, char* argv[])
 {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
+	std::cout << PROJECT_NAME << " " << VER << " by " << AUTHOR << std::endl;
 
-    // run the program as long as the window is open
-    while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-    }
+	if (argc > 1)
+	{
+		std::string romPath = argv[1];
+		Nes* nes = Nes::getInstance(romPath);
+	}
 
-    return 0;
+	return 0;
 }
