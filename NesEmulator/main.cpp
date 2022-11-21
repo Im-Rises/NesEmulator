5217ba36c9d19b7abefa4cb0cee7d88ba501ce9a
Im-Rises <quentin-morel88@hotmail.com>
2022-11-21 15:12:50 -0500

    Compiling SDL2 for windows and linux

--- NesEmulator/main.cpp
+++ NesEmulator/main.cpp
@@ -1,30 +1,55 @@
+#include <SDL.h>
 #include <iostream>
 
-#include "../NesEmulator/NesEmuUi.h"
-#include <SDL.h>
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
-/// <summary>
-/// Errors :
-/// 1 - Target rom not found
-/// 2 - Unknown cartridge (mapper not implemented)
-/// </summary>
-/// <param name="argc"></param>
-/// <param name="argv"></param>
-/// <returns></returns>
+int main(int arc, char ** argv) {
 
-/// <summary>
-/// To Do list:
-/// - Re-implement mappers
-/// </summary>
-/// <param name="argc"></param>
-/// <param name="argv"></param>
-/// <returns></returns>
+    if (SDL_Init( SDL_INIT_VIDEO ) < 0) {
+        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+    } else {
 
-int main(int argc, char* argv[]) {
-    std::cout << PROJECT_NAME << " " << VER << " by " << AUTHOR << std::endl;
+        SDL_CreateWindow(
+            "SDL2 Demo",
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+            SCREEN_WIDTH, SCREEN_HEIGHT,
+            SDL_WINDOW_SHOWN
+        );
 
-    NesEmuUi nesEmuUi;
-    nesEmuUi.start();
+        SDL_Delay(2000);
+    }
 
     return 0;
 }
+
+//#include <iostream>
+//
+//#include "../NesEmulator/NesEmuUi.h"
+//#include <SDL.h>
+//
+///// <summary>
+///// Errors :
+///// 1 - Target rom not found
+///// 2 - Unknown cartridge (mapper not implemented)
+///// </summary>
+///// <param name="argc"></param>
+///// <param name="argv"></param>
+///// <returns></returns>
+//
+///// <summary>
+///// To Do list:
+///// - Re-implement mappers
+///// </summary>
+///// <param name="argc"></param>
+///// <param name="argv"></param>
+///// <returns></returns>
+//
+//int main(int argc, char* argv[]) {
+//    std::cout << PROJECT_NAME << " " << VER << " by " << AUTHOR << std::endl;
+//
+//    NesEmuUi nesEmuUi;
+//    nesEmuUi.start();
+//
+//    return 0;
+//}
