ee4e8fef30311867267d9919cf383f5090d81b39
Im-Rises <quentin-morel88@hotmail.com>
2022-11-21 22:24:17 -0500

    Corrected test compilation

--- test/cpuTest.cpp
+++ test/cpuTest.cpp
@@ -7,17 +7,18 @@
 using namespace std;
 
 int main() {
-//	Cpu cpu;
-//	Cartridge cartridge("../../../../test_roms/cpu/nestest.nes");
-//	Bus bus(&cpu, &cartridge, nullptr);
-//	cpu.setPc(0xC000);
-//
-//	while (cpu.getPc() !=0xC66E){
-//		cpu.clock();
-//	}
-//
-//	const int add02 = bus.read(0x02);
-//	const int add03 = bus.read(0x03);
-//
-//	return add02 != 0;
+    Cpu cpu;
+    Cartridge cartridge("test_roms/cpu/nestest.nes");
+    Bus bus(&cpu, &cartridge, nullptr);
+    cpu.setPc(0xC000);
+
+    while (cpu.getPc() != 0xC66E)
+    {
+        cpu.clock();
+    }
+
+    const int add02 = bus.read(0x02);
+    const int add03 = bus.read(0x03);
+
+    return add02 != 0 && add03 != 0;
 }
